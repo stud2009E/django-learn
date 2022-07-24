d456e718953dbf444aed4fb3e62032fbfcb77cb9
unknown <stud2009@yandex.ru>
2022-07-24 18:06:20 +0300

    feat: add tests

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
@@ -11,13 +12,18 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-	model = Question
-	template_name = 'polls/detail.html'
+    model = Question
+    template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
 
 class ResultView(generic.DetailView):
 	model = Question
